88ab54148f32c79f10e82c54d738fa1be9350629
Brandon Cash <brandoncash@gmail.com>
2019-12-24 01:56:55 -0600

    Add scroll-based scaling and preloading to header block images.

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -6,14 +6,59 @@ export default class HeaderBlock extends Vue {
   @Prop(String) private title!: string;
   @Prop(String) private subtitle!: string;
   @Prop(String) private image!: string;
+  private windowScroll: number = 0;
+  private loaded: boolean = false;
+
+  private created() {
+    this.windowScroll = window.scrollY;
+    window.addEventListener('scroll', this.eventHandler);
+
+    const img = new Image();
+    img.src = this.image;
+
+    img.onload = () => {
+      this.loaded = true;
+    };
+  }
+
+  private destroyed() {
+    window.removeEventListener('scroll', this.eventHandler);
+  }
+
+  private eventHandler() {
+    // RAF helps ensure a higher framerate
+    window.requestAnimationFrame(() => {
+      this.windowScroll = window.scrollY;
+    });
+  }
+
+  get scaleAmount() {
+    // Never scale below 1.0
+    if (this.windowScroll < 0) {
+      return 1;
+    }
+
+    // High scale amount on small screens
+    if (window.innerWidth < 600) {
+      return 1 + (this.windowScroll * 0.001);
+    }
+
+    // Low scale amount on larger screens
+    return 1 + (this.windowScroll * 0.0002);
+  }
 }
 </script>
 
 <template>
-  <header
-    class="header-block"
-    :style="`background-image: url(${image});`"
-  >
+  <header class="header-block">
+    <img
+      class="background"
+      :class="{
+        loaded: loaded,
+      }"
+      :src="image"
+      :style="`transform: scale(${scaleAmount});`"
+    />
     <h1 class="title">{{ title }}</h1>
     <h2 class="subtitle">{{ subtitle }}</h2>
   </header>
@@ -22,13 +67,12 @@ export default class HeaderBlock extends Vue {
 <style lang="scss" scoped>
 .header-block {
   z-index: -1;
+  position: relative;
   width: 100%;
-  height: 39.25vw;
+  height: 39.5vw;
   margin-bottom: -10vw;
   padding: 3vw;
-  background-position: 100% 0%;
-  background-size: contain;
-  background-repeat: no-repeat;
+  overflow: hidden;
   background-color: $blue;
   color: $white;
   text-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
@@ -43,11 +87,28 @@ export default class HeaderBlock extends Vue {
   }
 }
 
+.background {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  z-index: 1;
+  transform-origin: 50% 100%;
+  opacity: 0;
+  transition: transform 50ms linear,
+    opacity 1s ease-in-out;
+
+  &.loaded {
+    opacity: 1;
+  }
+}
+
 .title {
+  position: relative;
   margin: 0;
   font-family: Montserrat;
   font-size: 3rem;
-  z-index: 1;
+  z-index: 2;
 
   @include breakpoint('md') {
     font-size: 4vw;
@@ -55,13 +116,14 @@ export default class HeaderBlock extends Vue {
 }
 
 .subtitle {
+  position: relative;
   margin: 0;
   font-family: Merriweather;
   font-size: 1.6rem;
   font-weight: 100;
   font-style: italic;
   text-transform: lowercase;
-  z-index: 1;
+  z-index: 2;
 
   @include breakpoint('md') {
     font-size: 2vw;
